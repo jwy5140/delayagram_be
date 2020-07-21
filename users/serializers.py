6cf768926d6173db5a7065b267e1844d38bc7961
jwy5140 <jwy5140@gmail.com>
2020-07-21 18:44:35 -0400

    Set up basic urls, corrected user serializer typo.

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import User
 
-class UserSerializer(serializers.ModelSerializer)
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        field = ('username', 'password', 'email', 'creation_date')
+        fields = ('username', 'password', 'email', 'creation_date')
