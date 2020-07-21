82b6ea8d04526bf5aaba39be3da5d526385d4322
jwy5140 <jwy5140@gmail.com>
2020-07-20 23:55:47 -0400

    Added user class.

--- users/models.py
+++ users/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+class User(models.Model):
+    username = models.CharField(max_length=16, unique=True)
+    password = models.CharField(max_length=16)
+    email = models.EmailField()
+    creation_date = models.DateTimeField(auto_add_now=True)
