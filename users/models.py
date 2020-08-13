c362d898150463169cc31c2bbb0553644755a6f8
jwy5140 <jwy5140@gmail.com>
2020-08-13 18:46:59 -0400

    Added full names to user model.

--- users/models.py
+++ users/models.py
@@ -6,4 +6,5 @@ class User(models.Model):
     username = models.CharField(max_length=16, unique=True)
     password = models.CharField(max_length=16)
     email = models.EmailField()
+    full_name = models.CharField(max_length=100)
     creation_date = models.DateTimeField(auto_now_add=True)
--- users/serializers.py
+++ users/serializers.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'password', 'email', 'creation_date')
+        fields = ('username', 'password', 'email', 'full_name', 'creation_date')
